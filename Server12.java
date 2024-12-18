06ed2c511d1ffcc9f2c67dfd69984ee179ea5087
SivaBalajiEevana-12 <eevansivabalaji@gmail.com>
2024-12-19 00:19:17 +0530

    server

---
+++ Server12.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+public class Server12 {
+    public static void main(String[] args) throws Exception{
+        ServerSocket ss = new ServerSocket(6000); 
+  Socket s = ss.accept(); 
+  DataInputStream dis = new DataInputStream(s.getInputStream()); 
+  DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+  String stream=""; 
+  String input=""; 
+  while(!stream.equals("stop")) 
+  { 
+   stream = dis.readUTF(); 
+   System.out.println("Client says: " + stream); 
+   input = br.readLine(); 
+   dos.writeUTF(input); 
+   dos.flush(); 
+  } 
+  dis.close(); 
+  dos.close(); 
+  s.close(); 
+ss.close();
+    }
+}
